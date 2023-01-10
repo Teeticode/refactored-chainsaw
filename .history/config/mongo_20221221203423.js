0c60f031ca3a34626f2fc65a19198710da623a09
Teeticode <teetinormm@gmail.com>
2023-01-10 22:32:10 +0300

    second commit

--- .history/config/mongo_20221221203423.js
+++ .history/config/mongo_20221221203423.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config()
-const MONGO_URI = process.env.MONGO_URI
+const MONGO_URI = process.env.MONGO_LOCAL
 mongoose.set('strictQuery',false)
 const connectToDb = async ()=>{
     try{
