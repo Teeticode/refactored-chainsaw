04962fa6e929d4b22deed9eac20fe2ec10a4858d
Teeticode <teetinormm@gmail.com>
2023-04-25 19:10:27 +0300

    commit of origins

---
+++ .history/server_20230424110010.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectToDb = require('./config/mongo');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const userRouter = require('./routes/userRoutes');
+const aboutRouter = require('./routes/aboutRoutes');
+const skillsRouter = require('./routes/skillsRouter');
+const levelsRouter = require('./routes/levelsRouter');
+const paymentRouter = require('./routes/paymentsRoute');
+const productsRouter = require('./routes/productsRoutes');
+const categoryRouter = require('./routes/categoryRoutes');
+const storeRouter = require('./routes/storeRoutes')
+
+const cookieParser = require('cookie-parser')
+const path = require('path')
+const {dirname} = require('path');
+const cors = require('cors')
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+const url = process.env.URL
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.use(cors());
+app.use(cookieParser())
+app.options('*', cors());
+app.use(morgan('tiny'))
+app.use(express.static(path.join(__dirname, 'Images')))
+app.use(`${url}+users`, userRouter);
+app.use(`${url}+about`, aboutRouter);
+app.use(`${url}+skills`, skillsRouter);
+app.use(`${url}+levels`, levelsRouter);
+app.use(`${url}+payments`, paymentRouter);
+app.use(`${url}+products`, productsRouter);
+app.use(`${url}+category`, categoryRouter);
+app.use(`${url}+stores`, storeRouter);
+app.use(`${url}+Images`, express.static(path.join(__dirname, 'Images')))
+
+connectToDb();
+app.get(`${url}`,(req,res)=>{
+    res.status(200).json({message:'Wooza'})
+    
+})
+app.listen(PORT,()=>{
+    console.log(`server is running on PORT ${PORT}`)
+})
